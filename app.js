8f40dbbf1628f86f569b6ec6c85a784f2ac48035
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-02-20 18:34:03 +0530

    fix: handle upperCase issues

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 var cors = require('cors')
 app.use(cors())
 
+const {JSDOM} = require('jsdom');
+
 var fConverted = null;
 var fConvertedHtml = null;
 app.set('view engine', 'hbs')
@@ -48,6 +50,7 @@ app.post("/inline-css", (req, res) => {
   const { data } = req.body
   if(data != null && data != undefined){
     var convertedData = replaceInlineCss(data);
+    convertedData = handleLowerCaseIssue(req.body.data, finalHtml, convertedData);
     var styles = convertedData.slice(convertedData.indexOf('<style>')+7,convertedData.indexOf('</style>'))
     var finalHtml = convertedData.slice(convertedData.indexOf('<body>')+6,convertedData.indexOf('</body>'))
     fConverted = styles
@@ -60,6 +63,26 @@ app.post("/inline-css", (req, res) => {
   }  
 })
 
+
+function handleLowerCaseIssue(oldText, newText, convertedData){
+  var newStr = convertedData;
+  var document = new JSDOM(newStr).window.document;
+  var doc = document.documentElement;
+  const elements = doc.getElementsByTagName('*');
+  for (let i = 0; i < elements.length; i++) {
+    var attributes = elements[i].attributes;
+    if(attributes){
+      for (let j = 0; j < attributes.length; j++) {
+        var index = oldText.toLocaleLowerCase().indexOf(attributes[j].name);
+        if (index !== -1) {
+          const extractedString = oldText.substr(index, attributes[j].name.length);
+          newStr = newStr.replaceAll(attributes[j].name, extractedString);
+        }
+      }
+    }
+  }
+  return newStr
+}
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
